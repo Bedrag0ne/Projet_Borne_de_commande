bb7f286b856319e7780afc85ecf8b0c3a8053f15
Bedrag0ne <georges1.colas@gmail.com>
2026-05-05 18:28:23 +0200

    fix port for render

--- server.js
+++ server.js
@@ -1,17 +1,19 @@
 const express = require('express');
 const app = express();
 
+const PORT = process.env.PORT || 3000;
+
 const productsRoutes = require('./routes/productsRoutes');
 const ordersRoutes = require('./routes/ordersRoutes');
 
 app.use(express.json());
-app.use(express.static('./views'));
+app.use(express.static('views'));
 
 app.use('/api', productsRoutes);
 app.use('/api', ordersRoutes);
 
 
 
-app.listen(3000, () => {
-  console.log('Server is running on http://localhost:3000');
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
